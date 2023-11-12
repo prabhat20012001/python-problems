27d7a2f0ba5ab1aa9a6b929ac53b9293b18da23d
Pabhat Kumar <prabhatkumarmnnit@gmail.com>
2023-11-12 22:03:42 +0530

    problems solved 1 and 2

---
+++ p1.py
@@ -0,0 +1,10 @@
+print("Number \t\t  Square")
+print("1 \t\t "+str(1*1))
+print("2 \t\t "+str(2*2))
+print("3 \t\t "+str(3*3))
+print("4 \t\t "+str(4*4))
+print("5 \t\t "+str(5*5))
+print("6 \t\t "+str(6*6))
+print("7 \t\t "+str(7*7))
+
+
